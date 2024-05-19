8538284c73db26c3ea8c787fd35a51cce3a93e4d
Mateus Santos <mateuspele2015@gmail.com>
2024-05-18 21:02:12 -0300

    Modify:  app.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import cepEstados from './utils/CepEstados';
+import dados from './utils/Dados';
 
-function App() {
+interface Local {
+  endereco: string;
+  cidade: string;
+  estado: string;
+  cep: string;
+}
+
+const App: React.FC = () => {
+  const [local, setLocal] = useState<Local | any>({
+    endereco: '',
+    cidade: '',
+    estado: '',
+    cep: ''
+  });
+  const [ip, setIp] = useState('');
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    axios.get('https://api.ipify.org?format=json')
+      .then(response => {
+        setIp(response.data.ip);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar o IP:', error);
+      });
+  }, []);
+
+
+  const consultCep = async (cep: any) => {
+    try {
+      const index = Math.floor(Math.random() * cepEstados[cep]?.length);
+      const newArray = cepEstados[cep]
+
+      setUrl(`https://viacep.com.br/ws/${newArray[index]}/json/`)
+      const response = await axios.get(`https://viacep.com.br/ws/${newArray[index]}/json/`);
+      setLocal({
+        ...local,
+        endereco: response.data.logradouro,
+        cidade: response.data.localidade,
+        estado: response.data.uf,
+        cep: response.data.cep,
+        ddd: response.data.ddd,
+
+      });
+
+    } catch (error) {
+      console.error('Erro ao buscar o CEP:', error);
+      setLocal([])
+    }
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-5 bg-gray-50 h-screen'>
+      <h1 className='font-medium text-lg'>Gerador de Estados e DDDs</h1>
+
+
+      <form className='flex gap-2 flex-col bg-gray-50 mt-5'>
+        <div className='flex gap-3'>
+          <input className='rounded-md p-1 border outline-blue-600 px-2 cursor-pointer' disabled type="text" placeholder="Endereço" value={local.endereco} />
+          <input className='rounded-md p-1 border outline-blue-600 px-2 cursor-pointer' disabled type="text" placeholder="Cidade" value={local.cidade} />
+        </div>
+
+        <div className='flex gap-3'>
+          <input className='rounded-md p-1 border outline-blue-600 px-2 cursor-pointer' disabled type="text" placeholder="Estado" value={local.estado} />
+          <input className='rounded-md p-1 border outline-blue-600 px-2 cursor-pointer' disabled type="text" placeholder="CEP" value={local.cep} />
+        </div>
+
+        <div className='flex gap-3'>
+          <input className='rounded-md p-1 border outline-blue-600 px-2 cursor-pointer' disabled type="text" placeholder="DDD" value={local.ddd} />
+        </div>
+      </form>
+      {false && <a href={url}>{url}</a>
+      }      <div className='mt-8'>
+        <h3>Lista de Estados e DDDs:</h3>
+        <button className='flex flex-wrap gap-2 border p-2 rounded-lg py-5 mt-5'>
+          {dados.map((item, index) => (
+            <div onClick={() => consultCep(item.uf)} className='bg-blue-800 hover:bg-blue-500 hover:text-ellipsis active:outline-blue-500 active:outline text-gray-200 mb-2 p-1 px-2 rounded-lg' key={index}>
+              <p>{item.estado} : {item.ddds}</p>
+            </div>
+          ))}
+        </button>
+      </div>
+
+      <div>
+        <h3 className='flex gap-2'>Seu IP é: <p className='text-green-500 animate-pulse'>{ip}</p></h3>
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/utils/Dados.ts
@@ -0,0 +1,39 @@
+
+interface Dados {
+    uf: string;
+    estado: string;
+    ddds: string;
+  }
+  
+  const dados: Dados[] = [
+    { uf: "AC", estado: "Acre", ddds: "68" },
+    { uf: "AL", estado: "Alagoas", ddds: "82" },
+    { uf: "AP", estado: "Amapá", ddds: "96" },
+    { uf: "AM", estado: "Amazonas", ddds: "92, 97" },
+    { uf: "BA", estado: "Bahia", ddds: "71, 73, 74, 75, 77" },
+    { uf: "CE", estado: "Ceará", ddds: "85, 88" },
+    { uf: "DF", estado: "Distrito Federal", ddds: "61" },
+    { uf: "ES", estado: "Espírito Santo", ddds: "27, 28" },
+    { uf: "GO", estado: "Goiás", ddds: "62, 64" },
+    { uf: "MA", estado: "Maranhão", ddds: "98, 99" },
+    { uf: "MT", estado: "Mato Grosso", ddds: "65, 66" },
+    { uf: "MS", estado: "Mato Grosso do Sul", ddds: "67" },
+    { uf: "MG", estado: "Minas Gerais", ddds: "31, 32, 33, 34, 35, 37, 38" },
+    { uf: "PA", estado: "Pará", ddds: "91, 93, 94" },
+    { uf: "PB", estado: "Paraíba", ddds: "83" },
+    { uf: "PR", estado: "Paraná", ddds: "41, 42, 43, 44, 45, 46" },
+    { uf: "PE", estado: "Pernambuco", ddds: "81, 87" },
+    { uf: "PI", estado: "Piauí", ddds: "86, 89" },
+    { uf: "RJ", estado: "Rio de Janeiro", ddds: "21, 22, 24" },
+    { uf: "RN", estado: "Rio Grande do Norte", ddds: "84" },
+    { uf: "RS", estado: "Rio Grande do Sul", ddds: "51, 53, 54, 55" },
+    { uf: "RO", estado: "Rondônia", ddds: "69" },
+    { uf: "RR", estado: "Roraima", ddds: "95" },
+    { uf: "SC", estado: "Santa Catarina", ddds: "47, 48, 49" },
+    { uf: "SP", estado: "São Paulo", ddds: "11, 12, 13, 14, 15, 16, 17, 18, 19" },
+    { uf: "SE", estado: "Sergipe", ddds: "79" },
+    { uf: "TO", estado: "Tocantins", ddds: "63" }
+  ];
+
+  export default dados;
+  
